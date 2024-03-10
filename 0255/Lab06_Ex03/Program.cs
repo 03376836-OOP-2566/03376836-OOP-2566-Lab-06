11a68e7f4b07100972f8d8fc2056da1b012ddaae
Sorawit255 <65030255@kmitl.ac.th>
2024-03-10 20:07:25 +0700

    3

---
+++ 0255/Lab06_Ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿Person person = new Person();
+person.name = "Enter your name here";
+System.Console.WriteLine(person.GetName());
+Student student = new Student();
+student.name = "Enter student's name";
+System.Console.WriteLine(student.GetName());
+
+class Person
+{
+    public string? name;
+
+    public string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+
+}
+
