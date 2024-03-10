cb8d542ad8b88b1d6facef6c6e86920addd45133
Sorawit255 <65030255@kmitl.ac.th>
2024-03-10 21:00:23 +0700

    9

---
+++ 0255/Lab06_Ex09/Program.cs
@@ -0,0 +1,10 @@
+﻿Circle.PrintCircleArea(100);
+class Circle
+{
+    const double PI = 3.14159;
+    public static void PrintCircleArea(double radius)
+    {
+        const double PI2 = PI + 0.00000265359; // change PI to more precision number.
+        System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+}
