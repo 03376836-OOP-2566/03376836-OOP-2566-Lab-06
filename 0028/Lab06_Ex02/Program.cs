0fadf8a92ff565c9dec88d00b9b457906b94845e
likunzz <65030028@kmitl.ac.th>
2024-03-14 15:07:54 +0700

    2

---
+++ 0028/Lab06_Ex02/Program.cs
@@ -0,0 +1,11 @@
+﻿Person.name = "Ping lnwza 007";
+System.Console.WriteLine(Person.GetName());
+
+static class Person
+{
+    static public  string? name;
+    static public  string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
