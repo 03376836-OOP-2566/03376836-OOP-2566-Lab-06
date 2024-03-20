378c04dd9ddd533476ed3a72552fe82cc6dfb0af
likunzz <65030028@kmitl.ac.th>
2024-03-20 19:09:59 +0700

    6

---
+++ 0028/Lab06_Ex06/Program.cs
@@ -0,0 +1,10 @@
+﻿Circle.PrintCircleArea(100);
+
+class Circle
+{
+    const double PI = 3.14159;
+    public static void PrintCircleArea(double radius)
+    {
+        System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+}
