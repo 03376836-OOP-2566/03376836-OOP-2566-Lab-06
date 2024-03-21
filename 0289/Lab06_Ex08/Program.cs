7946eeed2e155ff8986b40fda186ccf26e3b1739
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 02:17:40 +0700

    03376836-OOP-2566-Lab-06

---
+++ 0289/Lab06_Ex08/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Circle.PrintCircleArea(100);
+class Circle
+{
+    const double PI = 3.14159;
+    public static void PrintCircleArea(double radius)
+    {
+        PI = 3.14159265359; // change PI to more precision number.
+        System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+}
