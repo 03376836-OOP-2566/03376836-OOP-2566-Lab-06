e65229848f951ba13cef185e31e8f0278aefa825
AnchisaPhetnoi <65030289@kmitl.ac.th>
2024-03-22 01:57:31 +0700

    03376836-OOP-2566-Lab-06

---
+++ 0289/Lab06_Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Person.name = "Enter your name here";
+System.Console.WriteLine(Person.GetName());
+var student = new Student();
+student.name = "Enter student's name";
+System.Console.WriteLine(student.GetName());
+
+static class Person
+{
+    static  public  string? name;
+    static  public string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    
+}
