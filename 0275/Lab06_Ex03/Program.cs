6ac443e83d79a7c58c0ec8f8ad39a10ef434be1b
fulk2014 <144195753+fulk2014@users.noreply.github.com>
2024-03-22 16:12:04 +0700

    65030275_lab06

---
+++ 0275/Lab06_Ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿Person.name = "Enter your name here";
+System.Console.WriteLine(Person.GetName());
+var student = new Student();
+student.name = "Enter student's name";
+System.Console.WriteLine(student.GetName());
+
+static class Person
+{
+    public static string? name;
+    public static string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    public new static string GetName()
+    {
+        return $"Hello from student {name}";
+    }
+}
