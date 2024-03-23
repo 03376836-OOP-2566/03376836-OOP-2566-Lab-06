f8b42539234cfc3e6f9d481abb16be0b0ddaecb6
nitiphum <65030130@kmitl.ac.th>
2024-03-23 23:37:04 +0700

    commitex4

--- Lab06_Ex04/Program.cs
+++ Lab06_Ex04/Program.cs
@@ -3,12 +3,12 @@ var a2 = new AAA();
 a1.b1 = 10;
 AAA.b2 = 20;
 a2.b1 = 30;
-a2.b2 = 40;
+AAA.b2 = 40;
 
 System.Console.WriteLine($"a1.b1 = {a1.b1}");
 System.Console.WriteLine($"AAA.b2 = {AAA.b2}");
 System.Console.WriteLine($"a2.b1 = {a2.b1}");
-System.Console.WriteLine($"a2.b2 = {a2.b2}");
+System.Console.WriteLine($"a2.b2 = {AAA.b2}");
 
 class AAA 
 {
