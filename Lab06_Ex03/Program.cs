3931341bfebf85716e25cd43f28859a35f242bee
nitiphum <65030130@kmitl.ac.th>
2024-03-23 23:06:59 +0700

    สร้างex3

---
+++ Lab06_Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿Person.name = "Enter your name here";
+System.Console.WriteLine(Person.GetName());
+var student = new Student();
+student.name = "Enter student's name";
+System.Console.WriteLine(student.GetName());
+
+static class Person
+{
+    static  public  string? name;
+    static  public string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+    
+}
