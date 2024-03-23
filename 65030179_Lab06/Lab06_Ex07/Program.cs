d5182e294b3dbc4fac5fa5b399cac6ed3459f087
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 00:37:28 +0700

    commit Ex07

--- 65030179_Lab06/Lab06_Ex07/Program.cs
+++ 65030179_Lab06/Lab06_Ex07/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double r = 100.0;
+Circle.PrintCircleArea(r);
+Circle.PrintCircleCircumference(r);
+class Circle
+{
+    const double PI = 3.14159;
+    const double PI2 = PI * 2.0;
+    public static void PrintCircleArea(double radius)
+    {
+        System.Console.WriteLine($"Radius = {radius}, Area = {PI * radius * radius}");
+    }
+    public static void PrintCircleCircumference(double radius)
+    {
+        System.Console.WriteLine($"Radius = {radius}, Circumference = {PI2 * radius}");
+    }
+}
