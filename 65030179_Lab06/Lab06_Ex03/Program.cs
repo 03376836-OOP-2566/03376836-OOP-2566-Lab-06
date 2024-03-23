f750eb88594ad9fe8440b3d8bcf91db33c6100e3
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 00:14:09 +0700

    commit Ex03

--- 65030179_Lab06/Lab06_Ex03/Program.cs
+++ 65030179_Lab06/Lab06_Ex03/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Person.name = "shed0!";
+System.Console.WriteLine(Person.GetName());
+var student = new Student();
+Student.name = "Pattarapon Ponkul";
+System.Console.WriteLine(Student.GetName());
+
+
+class Person
+{
+    public static string? name;
+    public static string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
+
+class Student : Person
+{
+
+}
