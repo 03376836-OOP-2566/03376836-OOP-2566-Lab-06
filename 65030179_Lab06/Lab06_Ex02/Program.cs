057597ea1e75b547ce8f49e9e04f24a4bae492ea
PATTARAPON PONKUL <65030179@kmitl.ac.th>
2024-03-24 00:05:53 +0700

    commit Ex02

--- 65030179_Lab06/Lab06_Ex02/Program.cs
+++ 65030179_Lab06/Lab06_Ex02/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Person.name = "Enter your name here";
+System.Console.WriteLine(Person.GetName());
+
+static class Person
+{
+    static public  string? name;
+    static public  string GetName()
+    {
+        return $"Hello from {name}";
+    }
+}
