5646acbb88895943a75d972bb4fdc94c7aee9756
nitiphum <65030130@kmitl.ac.th>
2024-03-23 23:28:21 +0700

    commitex3

--- Lab06_Ex03/Program.cs
+++ Lab06_Ex03/Program.cs
@@ -1,13 +1,15 @@
-﻿Person.name = "Enter your name here";
+﻿// See https://aka.ms/new-console-template for more information
+Person.name = "Enter your name here";
 System.Console.WriteLine(Person.GetName());
 var student = new Student();
-student.name = "Enter student's name";
-System.Console.WriteLine(student.GetName());
+Student.name = "Enter student's name";
+System.Console.WriteLine(Student.GetName());
 
-static class Person
+
+class Person
 {
-    static  public  string? name;
-    static  public string GetName()
+    public static string? name;
+    public static string GetName()
     {
         return $"Hello from {name}";
     }
