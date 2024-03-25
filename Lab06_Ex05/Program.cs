1a288a70f0ac00ca624153195f631f699b116287
Khemmasedth <khmasade555@gmail.com>
2024-03-26 02:55:00 +0700

    finnish

---
+++ Lab06_Ex05/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+StaticDemo.i = 123;
+StaticDemo.f = 1234.56f;
+StaticDemo.s = "Hello World!";
+StaticDemo.PrintValues();
+
+class StaticDemo
+{
+    public static int i;
+    public static float f;
+    public static string s;
+    public static void PrintValues()
+    {
+        System.Console.WriteLine($"Integer i : {i}, floating point f : {f}, string s  : {s}");
+    }
+}
